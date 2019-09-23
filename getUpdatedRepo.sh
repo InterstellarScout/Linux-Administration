145585a3e0b25e36b3d2806aea93db9664d736a6
Dean's Github <das097@gmail.com>
2019-09-23 18:40:24 -0400

    Updated Files

---
+++ getUpdatedRepo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#Remove this file from the folder, drop it where you want the Repo (for example, Home or ~), then run this script (see below).
+#NOTE Permissions may be needed. If so, run chmod 777 getUpdatedRepo.sh and then run with ./getUpdatedRepo.sh
+sudo rm -r Linux-Administration
+git clone https://github.com/Intergalacticstoryteller/Linux-Administration.git
