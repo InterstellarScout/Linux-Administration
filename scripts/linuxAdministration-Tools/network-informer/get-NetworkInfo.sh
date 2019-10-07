bdfd0436975e3a0a77ede97c3986c98de3e8ce57
Dean's Github <das097@gmail.com>
2019-10-07 17:39:35 -0400

    Updated Files

--- scripts/linuxAdministration-Tools/network-informer/get-NetworkInfo.sh
+++ scripts/linuxAdministration-Tools/network-informer/get-NetworkInfo.sh
@@ -38,17 +38,17 @@ printf "\n\nMy WAN/Public IP address: ${myip}" >> $file
 #Get Internal IP Addresses
 ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-printf "\n$ip4"
-printf "\n$ip6"
-printf "\n$ip4" >> $file
-printf "\n$ip6" >> $file
+printf "\nYour local IPv4 address on interface eth0 is $ip4\n"
+printf "\nYour local IPv6 address on interface eth0 is $ip6\n"
+printf "\nYour local IPv4 address on interface eth0 is $ip4\n" >> $file
+printf "\nYour local IPv6 address on interface eth0 is $ip6\n" >> $file
 
-if [ "$ip4"=="127.0.0.1" ]; then
+if [ $ip4 == "127.0.0.1" ]; then
   echo Interface eth0 is not connected and has the address 127.0.0.1, a loopback address.
 else
   echo Interface eth0 has the IPv4 address $ip4
 fi
-if [ "$ip6"=="::1" ]; then
+if [ $ip6 == "::1" ]; then
   echo Interface eth0 IPv6 is not connected and has the address 127.0.0.1, a loopback address.
 else
   echo Interface eth0 has the IPv6 address $ip6
@@ -56,17 +56,17 @@ fi
 
 lo_ip4=$(/sbin/ip -o -4 addr list lo | awk '{print $4}' | cut -d/ -f1)
 lo_ip6=$(/sbin/ip -o -6 addr list lo | awk '{print $4}' | cut -d/ -f1)
-printf "\n$lo_ip4"
-printf "\n$lo_ip6"
-printf "\n$lo_ip4" >> $file
-printf "\n$lo_ip6" >> $file
+printf "\nYour local IPv4 address on interface lo is $lo_ip4\n"
+printf "\nYour local IPv6 address on interface lo is $lo_ip6\n"
+printf "\nYour local IPv4 address on interface lo is $lo_ip4\n" >> $file
+printf "\nYour local IPv6 address on interface lo is $lo_ip6\n" >> $file
 
-if [ "$lo_ip4"=="127.0.0.1" ]; then
+if [ $lo_ip4 == "127.0.0.1" ]; then
 echo Interface lo is not connected and has the address 127.0.0.1, a loopback address.
 else
   echo Interface lo has the IPv4 address $lo_ip4
 fi
-if [ "$lo_ip6"=="::1" ]; then
+if [ $lo_ip6 == "::1" ]; then
 echo Interface lo is not connected and has the address 127.0.0.1, a loopback address.
 else
   echo Interface lo has the IPv6 address $ip6
@@ -75,25 +75,25 @@ fi
 __='
 wlan0_ip4=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
 wlan0_ip6=$(/sbin/ip -o -6 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
-echo $wlan0_ip4
-echo $wlan0_ip6
-echo $wlan0_ip4 >> $file
-echo $wlan0_ip6 >> $file
+printf "\nYour local IPv4 address on interface wlan0 is $wlan0_ip4\n"
+printf "\nYour local IPv6 address on interface wlan0 is $wlan0_ip6\n"
+printf "\nYour local IPv4 address on interface wlan0 is $wlan0_ip4\n" >> $file
+printf "\nYour local IPv6 address on interface wlan0 is $wlan0_ip6\n" >> $file
 
-if [ "$wlan0_ip4"=="127.0.0.1" ]; then
+if [ $wlan0_ip4 == "127.0.0.1" ]; then
 echo Interface wlan0 is not connected and has the address 127.0.0.1, a loopback address.
 else
   echo Interface wlan0 has the IPv6 address $wlan0_ip4
   fi
-if [ "$wlan0_ip6"=="::1" ]; then
+if [ $wlan0_ip6 == "::1" ]; then
 echo Interface wlan0 is not connected and has the address 127.0.0.1, a loopback address.
 else
   echo Interface wlan0 has the IPv6 address $wlan0_ip6
   fi'
 
 MAC=$(ifconfig eth0 | grep -Eo ..\(\:..\){5})
-printf "\n\nHardware Address: $MAC"
-printf "\n\nHardware Address: $MAC" >> $file
+printf "\n\nHardware Address: $MAC\n"
+printf "\n\nHardware Address: $MAC\n" >> $file
 
 #Check for open ports - outputs open ports, what's listening, and if programs are using it.
 echo Open Ports:
