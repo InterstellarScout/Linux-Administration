c7153b369c14fb3359c53b2be5eec8aa8f8eb578
Dean Sheldon <das097@gmail.com>
2019-12-21 13:25:13 -0500

    Updated Project Files

--- getUpdatedRepo.sh
+++ getUpdatedRepo.sh
@@ -1,4 +1,4 @@
-getUpdatedRepo.sh#!/bin/sh
+#!/bin/sh
 #Remove this file from the folder, drop it where you want the Repo (for example, Home or ~), then run this script (see below).
 #NOTE Permissions may be needed. If so, run chmod 777 getUpdatedRepo.sh and then run with ./getUpdatedRepo.sh
 cd ~
